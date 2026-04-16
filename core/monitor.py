d68985119bccf77f1198b6ea5995006cd6c65722
deklan400 <fadlan400@gmail.com>
2026-04-17 00:02:22 +0700

    Refactor check_vps and check_all_vps functions

--- core/monitor.py
+++ core/monitor.py
@@ -1,9 +1,10 @@
 from services.ssh_client import get_node_status, get_node_reward
-from utils.parser import parse_reward
+from utils.parser import parse_reward, is_valid_output
 
 
-def check_vps(host):
+def check_vps(name, host):
     data = {
+        "name": name,
         "host": host,
         "status": "down",
         "reward": None
@@ -15,19 +16,21 @@ def check_vps(host):
 
     # cek reward kalau hidup
     if status == "running":
-        raw_reward = get_node_reward(host)
-        reward = parse_reward(raw_reward)
-        data["reward"] = reward
+        raw = get_node_reward(host)
+
+        if is_valid_output(raw):
+            reward = parse_reward(raw)
+            data["reward"] = reward
 
     return data
 
 
-def check_all_vps(vps_list):
+def check_all_vps(vps_dict):
     results = []
 
-    for host in vps_list:
-        print(f"[CHECK] {host}")
-        result = check_vps(host)
+    for name, host in vps_dict.items():
+        print(f"[CHECK] {name} ({host})")
+        result = check_vps(name, host)
         results.append(result)
 
     return results
