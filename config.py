34753d60e27334d2758749ae7216d92449f9e4c9
deklan400 <fadlan400@gmail.com>
2026-04-17 00:35:34 +0700

    Refactor config.py by removing dotenv and comments
    Removed dotenv loading and some comments.

--- config.py
+++ config.py
@@ -1,17 +1,10 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # ========================
-# VPS LIST
+# VPS LIST (WAJIB ISI)
 # ========================
 VPS_LIST = {
     "vps1": "root@IP_VPS_1",
     "vps2": "root@IP_VPS_2",
-    "vps3": "root@IP_VPS_3",
-    "vps4": "root@IP_VPS_4",
-    "vps5": "root@IP_VPS_5"
+    "vps3": "root@IP_VPS_3"
 }
 
 # ========================
@@ -19,9 +12,3 @@ VPS_LIST = {
 # ========================
 CHECK_INTERVAL = 1800      # 30 menit
 REPORT_INTERVAL = 10800    # 3 jam
-
-# ========================
-# TELEGRAM
-# ========================
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
