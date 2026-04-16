191facaf22f6e6d0c853b49d10342e1fc8b7195e
deklan400 <fadlan400@gmail.com>
2026-04-16 23:58:47 +0700

    Add time utility functions for current time and duration

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,1 +1,10 @@
+from datetime import datetime
 
+
+def now_str():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+def format_duration(seconds):
+    minutes = seconds // 60
+    return f"{minutes} menit"
