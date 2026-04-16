1a46ae6f531cbf6683d052604e62f2420a61a461
deklan400 <fadlan400@gmail.com>
2026-04-17 00:38:24 +0700

    Implement main function for monitoring VPS

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from core.scheduler import run
+from config import VPS_LIST
+from utils.logger import log
 
+
+def main():
+    log("=== OPTIMAI MONITOR STARTED ===")
+
+    try:
+        run(VPS_LIST)
+
+    except KeyboardInterrupt:
+        log("Stopped by user")
+
+    except Exception as e:
+        log(f"[ERROR] {e}")
+
+
+if __name__ == "__main__":
+    main()
