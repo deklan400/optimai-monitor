40a2ebfb7a52135359c678f6f2caca45d469ff32
deklan400 <fadlan400@gmail.com>
2026-04-16 23:50:43 +0700

    Update parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -1,1 +1,17 @@
+import re
 
+def parse_reward(output):
+    """
+    Extract number from:
+    Total rewards: 28945.55
+    """
+
+    if not output:
+        return None
+
+    match = re.search(r"Total rewards:\s*([0-9.]+)", output)
+
+    if match:
+        return float(match.group(1))
+
+    return None
