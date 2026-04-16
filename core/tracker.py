06b66d7a394eb48a3b0844787502c1dc9c549106
deklan400 <fadlan400@gmail.com>
2026-04-17 00:06:37 +0700

    Refactor JSON load/save functions with utils

--- core/tracker.py
+++ core/tracker.py
@@ -1,37 +1,20 @@
-import json
-import os
+from utils.file_manager import load_json_safe, save_json_safe
 
 STATE_FILE = "data/state.json"
 REWARD_FILE = "data/rewards.json"
 
 
-def load_json(file_path):
-    if not os.path.exists(file_path):
-        return {}
-
-    with open(file_path, "r") as f:
-        try:
-            return json.load(f)
-        except:
-            return {}
-
-
-def save_json(file_path, data):
-    with open(file_path, "w") as f:
-        json.dump(data, f, indent=4)
-
-
 def load_state():
-    return load_json(STATE_FILE)
+    return load_json_safe(STATE_FILE)
 
 
 def save_state(state):
-    save_json(STATE_FILE, state)
+    save_json_safe(STATE_FILE, state)
 
 
 def load_rewards():
-    return load_json(REWARD_FILE)
+    return load_json_safe(REWARD_FILE)
 
 
 def save_rewards(rewards):
-    save_json(REWARD_FILE, rewards)
+    save_json_safe(REWARD_FILE, rewards)
