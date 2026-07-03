6df23ae1f7de507b0013ae29a08afb3ae809cb7a
deklan400 <fadlan400@gmail.com>
2026-07-03 16:00:00 +0700

    Persist daily report rollover date

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,8 +1,9 @@
 import time
-from datetime import timedelta
+from datetime import date, timedelta
 
 from config import CHECK_INTERVAL, REPORT_INTERVAL
 from core.check_cycle import current_local_date, day_window_utc, run_check_cycle
+from core.tracker import load_daily_report_state, save_daily_report_state
 from services.telegram_bot import send_message
 from utils.logger import log
 
@@ -17,9 +18,24 @@ def _send_alerts(alerts):
         send_message(alert)
 
 
+def _load_last_local_date():
+    state = load_daily_report_state()
+    raw_date = state.get("active_date")
+
+    if raw_date:
+        try:
+            return date.fromisoformat(raw_date)
+        except ValueError:
+            pass
+
+    today = current_local_date()
+    save_daily_report_state(today.isoformat())
+    return today
+
+
 def run(vps_source):
     last_report_time = time.time()
-    last_local_date = current_local_date()
+    last_local_date = _load_last_local_date()
     baseline_ready = False
 
     while True:
@@ -55,7 +71,9 @@ def run(vps_source):
             _send_alerts(daily_alerts)
             if daily_report:
                 send_message(daily_report)
+
             last_local_date = current_date
+            save_daily_report_state(current_date.isoformat())
 
         now = time.time()
         if now - last_report_time >= REPORT_INTERVAL:
