7033c8493a07af26569a17f5e1203c18b336a194
deklan400 <fadlan400@gmail.com>
2026-07-04 09:44:53 +0700

    Bump activity chart asset version

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,14 +1,17 @@
+import re
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent
 INDEX = ROOT / "web" / "index.html"
 
-CSS = '<link rel="stylesheet" href="/static/activity_chart.css?v=1">'
-JS = '<script src="/static/activity_chart.js?v=1"></script>'
+CSS = '<link rel="stylesheet" href="/static/activity_chart.css?v=2">'
+JS = '<script src="/static/activity_chart.js?v=2"></script>'
 
 try:
     if INDEX.exists():
         html = INDEX.read_text(encoding="utf-8")
+        html = re.sub(r'\s*<link rel="stylesheet" href="/static/activity_chart\.css\?v=\d+">', '', html)
+        html = re.sub(r'\s*<script src="/static/activity_chart\.js\?v=\d+"></script>', '', html)
         changed = False
         if CSS not in html and "</head>" in html:
             html = html.replace("</head>", f"  {CSS}\n</head>", 1)
